93cc41c400f4ccbed132006e07508ea66b4c8272
hama2109 <aiman.hamzaoui@usherbrooke.ca>
2019-11-27 15:35:19 -0500

    changement de representation pour MCL

--- SRC/MCL.py
+++ SRC/MCL.py
@@ -4,7 +4,7 @@ import numpy as np
 labels = np.load("../DATA/simulation/01/labels.array.npy")
 distance_matrix = np.load("../DATA/simulation/01/identity_distance_graph1.array.npy") 
 
-print(distance_matrix)
+distance_matrix = (distance_matrix <0.5).astype(int)
 
 for inflation in [i / 10 for i in range(11, 26)]:
     result = mc.run_mcl(distance_matrix, inflation=inflation)
